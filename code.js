d6f9c4b0a48e7ddabfc6d5bb72fd458f0d3025e8
Caleb O'Kane <caleb.ceo.okane@gmail.com>
2022-08-24 15:12:56 -0500

    i think i did exercise #2 correct...

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ function helloWorld() {
     console.log("Hello, World!");
 }
 // sayHello function
-const sayHello = function() {
-    return "Hello, World!";
+const sayHello = function(input) {
+    return ("Hello, " + input);
 }
 //
