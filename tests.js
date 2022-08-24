dcb3cbc700d5a2ac1ec94bce30a07a20fd6cd311
Caleb O'Kane <caleb.ceo.okane@gmail.com>
2022-08-24 15:27:06 -0500

    I think i did #5 right...

--- tests.js
+++ tests.js
@@ -22,5 +22,11 @@ describe('sayHello', function() {
     });
     it('should return the string "Hello, Jane!" when executed', function() {
         expect(sayHello("Jane")).toBe("Hello, Jane!");
-    })
+    });
+    it('should return the string "Hello, Alex!" when executed', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return the string "Hello, Pat!" when executed', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
 })
