33a0752d76bec49f511b36a5d1ed7634484ff1b2
Caleb O'Kane <caleb.ceo.okane@gmail.com>
2022-08-24 16:42:34 -0500

    unit tests for edge cases.

--- tests.js
+++ tests.js
@@ -29,4 +29,13 @@ describe('sayHello', function() {
     it('should return the string "Hello, Pat!" when executed', function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
-})
+    it('should return the string "Hello, World!" when executed with no input', function() {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when input is true', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when input is false', function() {
+        expect(sayHello(false)).toBe("Hello, World!")
+    });
+});
